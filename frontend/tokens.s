f4a921f14f100281b0e5df1b62ef4f8c5e25a54a
shuwen <1201220707@pku.edu.cn>
2026-04-06 01:56:56 +0800

    Update frontend and VS Code support.

--- frontend/tokens.s
+++ frontend/tokens.s
@@ -54,6 +54,8 @@ func is_keyword(text: String) -> bool {
         "package" => true,
         "use" => true,
         "as" => true,
+        "func" => true,
+        "var" => true,
         "var" => true,
         "const" => true,
         "static" => true,
