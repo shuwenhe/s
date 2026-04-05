4233c86efcf30577a6d9eeb5cca6be7402eda956
shuwen <1201220707@pku.edu.cn>
2026-04-06 03:34:19 +0800

    Update compiler and standard library.

--- std/io.s
+++ std/io.s
@@ -1,5 +1,13 @@
 package std.io
 
-func println(text: String) -> () {}
+func println(text: String) -> () {
+    __host_println(text)
+}
 
-func eprintln(text: String) -> () {}
+func eprintln(text: String) -> () {
+    __host_eprintln(text)
+}
+
+extern "intrinsic" func __host_println(text: String) -> ()
+
+extern "intrinsic" func __host_eprintln(text: String) -> ()
