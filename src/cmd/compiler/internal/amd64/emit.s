6237b1fe6712270018c71e501fb27f323409cf41
shuwen <1201220707@pku.edu.cn>
2026-04-06 23:04:10 +0800

    auto: Update commands.

---
+++ src/cmd/compiler/internal/amd64/emit.s
@@ -0,0 +1,14 @@
+package compiler.internal.amd64
+
+use compiler.backend_elf64.BackendError
+use compiler.backend_elf64.EmitProgram
+use compiler.internal.ssagen.MachineProgram
+use std.result.Result
+
+func ArchName() -> String {
+    "amd64"
+}
+
+func LinkProgram(MachineProgram program, String outputPath) -> Result[(), BackendError] {
+    EmitProgram(program, outputPath)
+}
