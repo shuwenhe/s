e4d09182cd613fb5f58be63aa415842d07942679
shuwen <1201220707@pku.edu.cn>
2026-04-16 17:17:20 +0800

    fix:compile

--- src/cmd/compile/internal/build/build.s
+++ src/cmd/compile/internal/build/build.s
@@ -1,49 +1,36 @@
 package compile.internal.build
 
-use compile.internal.build.exec.ExecError
-use compile.internal.build.exec.Run as RunCommand
+use compile.internal.backend.Build as BuildBinary
+use compile.internal.backend.Run as RunBinary
 use compile.internal.build.parse.ParseOptions
-use compile.internal.build.parse.Usage
-use compile.internal.build.report.Error as ReportError
-use compile.internal.build.report.Usage as ReportUsage
-use std.result.Result
 use std.vec.Vec
 
 func Main(Vec[String] args) -> i32 {
-    match Run(args) {
-        Result::Ok(_) => 0,
-        Result::Err(err) => {
-            ReportError(err.message)
-            1
-        },
+    var options_result = ParseOptions(args)
+    if options_result.is_err() {
+        return 1
     }
-}
 
-func Run(Vec[String] args) -> Result[(), ExecError] {
-    match ParseOptions(args) {
-        Result::Ok(options) => {
-            if options.command == "help" {
-                ReportUsage(Usage())
-                Result::Ok(())
-            } else {
-                match RunCommand(options) {
-                    Result::Ok(_) => Result::Ok(()),
-                    Result::Err(err) => Result::Err(convert_exec_error(err.message)),
-                }
-            }
+    var options = options_result.unwrap()
+    if options.command == "help" {
+        return 0
+    }
+
+    if options.command == "build" {
+        var build_result = BuildBinary(options.path, options.output)
+        if build_result.is_ok() {
+            return 0
         }
-        Result::Err(err) => Result::Err(convert_parse_error(err.message)),
+        return 1
     }
-}
 
-func convert_parse_error(String message) ExecError {
-    ExecError {
-        message: message,
+    if options.command == "run" {
+        var run_result = RunBinary(options.path)
+        if run_result.is_ok() {
+            return 0
+        }
+        return 1
     }
-}
 
-func convert_exec_error(String message) ExecError {
-    ExecError {
-        message: message,
-    }
+    0
 }
--- src/cmd/compile/internal/gc/gc.s
+++ src/cmd/compile/internal/gc/gc.s
@@ -4,24 +4,18 @@ use compile.internal.arch.Init as archInit
 use compile.internal.build.Main as buildMain
 use internal.buildcfg.Check as buildcfgCheck
 use internal.buildcfg.GOARCH as buildcfgGOARCH
-use std.io.eprintln
 use std.vec.Vec
 
 func Main(Vec[String] args) -> i32 {
     var buildcfg_err = buildcfgCheck()
     if buildcfg_err != "" {
-        return fail(buildcfg_err)
-    } else {
-        var arch_err = archInit(buildcfgGOARCH())
-        if arch_err != "" {
-            return fail(arch_err)
-        } else {
-            return buildMain(args)
-        }
+        return 2
     }
-}
 
-func fail(String message) -> i32 {
-    eprintln("compile: " + message)
-    2
+    var arch_err = archInit(buildcfgGOARCH())
+    if arch_err != "" {
+        return 2
+    }
+
+    return buildMain(args)
 }
