6c207b0952bd9a0acb23154b300027a6b88055e7
shuwen <1201220707@pku.edu.cn>
2026-04-17 16:09:16 +0800

    fix: s: make ast.s parser-compatible and adjust dump formatting

--- src/s/ast.s
+++ src/s/ast.s
@@ -260,55 +260,64 @@ enum Item {
 }
 
 struct SourceFile {
-    String package,
+    String pkg,
     Vec[UseDecl] uses,
     Vec[Item] items,
 }
 
-func dump_source_file(SourceFile source) String {
+func dump_source_file(SourceFile source) -> String {
     var lines = Vec[String]()
-    lines.push("package " + source.package)
-    for use_decl in source.uses {
+    lines.push("package " + source.pkg);
+    var ui = 0
+    while ui < source.uses.len() {
+        var use_decl = source.uses[ui]
         var text =
             match use_decl.alias {
-                Option::Some(alias) => "use " + use_decl.path + " as " + alias,
-                Option::None => "use " + use_decl.path,
+                Option.Some(alias) => "use " + use_decl.path + " as " + alias,
+                Option.None => "use " + use_decl.path,
             }
-        lines.push(text)
+        lines.push(text);
+        ui = ui + 1
     }
-    for item in source.items {
-        append_item_dump(lines, item)
+    var ii = 0
+    while ii < source.items.len() {
+        var item = source.items[ii]
+        append_item_dump(lines, item);
+        ii = ii + 1
     }
     join_lines(lines)
 }
 
-func append_item_dump(Vec[String] lines, Item item) () {
+func append_item_dump(Vec[String] lines, Item item) -> () {
     match item {
-        Item::Function(value) => append_lines(lines, dump_function(value, "")),
-        Item::Struct(value) => append_lines(lines, dump_struct(value)),
-        Item::Enum(value) => append_lines(lines, dump_enum(value)),
-        Item::Trait(value) => append_lines(lines, dump_trait(value)),
-        Item::Impl(value) => append_lines(lines, dump_impl(value)),
+        Item.Function(value) => append_lines(lines, dump_function(value, "")),
+        Item.Struct(value) => append_lines(lines, dump_struct(value)),
+        Item.Enum(value) => append_lines(lines, dump_enum(value)),
+        Item.Trait(value) => append_lines(lines, dump_trait(value)),
+        Item.Impl(value) => append_lines(lines, dump_impl(value)),
     }
 }
 
-func fmt_generics(Vec[String] generics) String {
+func fmt_generics(Vec[String] generics) -> String {
     if len(generics) == 0 {
         return ""
     }
     "[" + join_with(generics, ", ") + "]"
 }
 
-func dump_function(FunctionDecl item, String indent) Vec[String] {
+func dump_function(FunctionDecl item, String indent) -> Vec[String] {
     var lines = Vec[String]()
     var params = Vec[String]()
-    for param in item.sig.params {
+    var _pi = 0
+    while _pi < item.sig.params.len() {
+        var param = item.sig.params[_pi]
         params.push(param.type_name + " " + param.name)
+        _pi = _pi + 1
     }
     var ret =
         match item.sig.return_type {
-            Option::Some(value) => " " + value,
-            Option::None => "",
+            Option.Some(value) => " -> " + value,
+            Option.None => "",
         }
     var prefix = if item.is_public { "pub " } else { "" }
     lines.push(
@@ -323,48 +332,59 @@ func dump_function(FunctionDecl item, String indent) Vec[String] {
             + ret
     )
     match item.body {
-        Option::Some(body) => append_lines(lines, dump_block(body, indent + "  ")),
-        Option::None => (),
+        Option.Some(body) => append_lines(lines, dump_block(body, indent + "  ")),
+        Option.None => (),
     }
     lines
 }
 
-func dump_struct(StructDecl item) Vec[String] {
+func dump_struct(StructDecl item) -> Vec[String] {
     var lines = Vec[String]()
     var prefix = if item.is_public { "pub " } else { "" }
     lines.push(prefix + "struct " + item.name + fmt_generics(item.generics))
-    for field in item.fields {
+    var _fi = 0
+    while _fi < item.fields.len() {
+        var field = item.fields[_fi]
         var fp = if field.is_public { "pub " } else { "" }
         lines.push("  " + fp + field.type_name + " " + field.name)
+        _fi = _fi + 1
     }
     lines
 }
 
-func dump_enum(EnumDecl item) Vec[String] {
+func dump_enum(EnumDecl item) -> Vec[String] {
     var lines = Vec[String]()
     lines.push("enum " + item.name + fmt_generics(item.generics))
-    for variant in item.variants {
+    var _vi = 0
+    while _vi < item.variants.len() {
+        var variant = item.variants[_vi]
         match variant.payload {
-            Option::Some(payload) => lines.push("  " + variant.name + "(" + payload + ")"),
-            Option::None => lines.push("  " + variant.name),
+            Option.Some(payload) => lines.push("  " + variant.name + "(" + payload + ")"),
+            Option.None => lines.push("  " + variant.name),
         }
+        _vi = _vi + 1
     }
     lines
 }
 
-func dump_trait(TraitDecl item) Vec[String] {
+func dump_trait(TraitDecl item) -> Vec[String] {
     var lines = Vec[String]()
     var prefix = if item.is_public { "pub " } else { "" }
     lines.push(prefix + "trait " + item.name + fmt_generics(item.generics))
-    for method in item.methods {
+    var _mi = 0
+    while _mi < item.methods.len() {
+        var method = item.methods[_mi]
         var params = Vec[String]()
-        for param in method.params {
+        var _mpi = 0
+        while _mpi < method.params.len() {
+            var param = method.params[_mpi]
             params.push(param.type_name + " " + param.name)
+            _mpi = _mpi + 1
         }
         var ret =
             match method.return_type {
-                Option::Some(value) => " " + value,
-                Option::None => "",
+                Option.Some(value) => " -> " + value,
+                Option.None => "",
             }
         lines.push(
             "  func "
@@ -379,50 +399,56 @@ func dump_trait(TraitDecl item) Vec[String] {
     lines
 }
 
-func dump_impl(ImplDecl item) Vec[String] {
+func dump_impl(ImplDecl item) -> Vec[String] {
     var lines = Vec[String]()
     var head =
         match item.trait_name {
-            Option::Some(name) => name + " for " + item.target,
-            Option::None => item.target,
+            Option.Some(name) => name + " for " + item.target,
+            Option.None => item.target,
         }
     var title = replace_once("impl " + fmt_generics(item.generics) + " " + head, "impl  ", "impl ")
     lines.push(title)
-    for method in item.methods {
+    var _mi2 = 0
+    while _mi2 < item.methods.len() {
+        var method = item.methods[_mi2]
         append_lines(lines, dump_function(method, "  "))
+        _mi2 = _mi2 + 1
     }
     lines
 }
 
-func dump_block(BlockExpr block, String indent) Vec[String] {
+func dump_block(BlockExpr block, String indent) -> Vec[String] {
     var lines = Vec[String]()
-    for stmt in block.statements {
+    var _si = 0
+    while _si < block.statements.len() {
+        var stmt = block.statements[_si]
         append_lines(lines, dump_stmt(stmt, indent))
+        _si = _si + 1
     }
     match block.final_expr {
-        Option::Some(expr) => lines.push(indent + "final " + dump_expr(expr)),
-        Option::None => (),
+        Option.Some(expr) => lines.push(indent + "final " + dump_expr(expr)),
+        Option.None => (),
     }
     lines
 }
 
-func dump_stmt(Stmt stmt, String indent) Vec[String] {
+func dump_stmt(Stmt stmt, String indent) -> Vec[String] {
     match stmt {
-        Stmt::Var(value) => {
+        Stmt.Var(value) => {
             var text =
                 match value.type_name {
-                    Option::Some(type_name) => indent + type_name + " " + value.name + " = " + dump_expr(value.value),
-                    Option::None => indent + "var " + value.name + " = " + dump_expr(value.value),
+                    Option.Some(type_name) => indent + type_name + " " + value.name + " = " + dump_expr(value.value),
+                    Option.None => indent + "var " + value.name + " = " + dump_expr(value.value),
             }
             single_line(text)
         }
-        Stmt::Assign(value) => {
+        Stmt.Assign(value) => {
             single_line(indent + value.name + " = " + dump_expr(value.value))
         }
-        Stmt::Increment(value) => {
+        Stmt.Increment(value) => {
             single_line(indent + value.name + "++")
         }
-        Stmt::CFor(value) => {
+        Stmt.CFor(value) => {
             var lines = Vec[String]()
             lines.push(
                 indent
@@ -437,53 +463,53 @@ func dump_stmt(Stmt stmt, String indent) Vec[String] {
             append_lines(lines, dump_block(value.body, indent + "  "))
             lines
         }
-        Stmt::Return(value) => {
+        Stmt.Return(value) => {
             var text =
                 match value.value {
-                    Option::Some(expr) => indent + "return " + dump_expr(expr),
-                    Option::None => indent + "return ()",
+                    Option.Some(expr) => indent + "return " + dump_expr(expr),
+                    Option.None => indent + "return ()",
                 }
             single_line(text)
         }
-        Stmt::Expr(value) => single_line(indent + "expr " + dump_expr(value.expr)),
-        Stmt::Defer(value) => single_line(indent + "defer " + dump_expr(value.expr)),
+        Stmt.Expr(value) => single_line(indent + "expr " + dump_expr(value.expr)),
+        Stmt.Defer(value) => single_line(indent + "defer " + dump_expr(value.expr)),
     }
 }
 
-func dump_for_clause(Stmt stmt) String {
+func dump_for_clause(Stmt stmt) -> String {
     match stmt {
-        Stmt::Var(value) => {
+        Stmt.Var(value) => {
             match value.type_name {
-                Option::Some(type_name) => type_name + " " + value.name + " = " + dump_expr(value.value),
-                Option::None => "var " + value.name + " = " + dump_expr(value.value),
+                Option.Some(type_name) => type_name + " " + value.name + " = " + dump_expr(value.value),
+                Option.None => "var " + value.name + " = " + dump_expr(value.value),
             }
         }
-        Stmt::Assign(value) => value.name + " = " + dump_expr(value.value),
-        Stmt::Increment(value) => value.name + "++",
-        Stmt::Expr(value) => dump_expr(value.expr),
-        Stmt::Return(_) => "return",
-        Stmt::CFor(_) => "for (...)",
+        Stmt.Assign(value) => value.name + " = " + dump_expr(value.value),
+        Stmt.Increment(value) => value.name + "++",
+        Stmt.Expr(value) => dump_expr(value.expr),
+        Stmt.Return(_) => "return",
+        Stmt.CFor(_) => "for (...)",
     }
 }
 
-func dump_expr(Expr expr) String {
+func dump_expr(Expr expr) -> String {
     match expr {
-        Expr::Int(value) => value.value,
-        Expr::String(value) => value.value,
-        Expr::Bool(value) => if value.value { "true" } else { "false" },
-        Expr::Name(value) => value.name,
-        Expr::Borrow(value) => {
+        Expr.Int(value) => value.value,
+        Expr.String(value) => value.value,
+        Expr.Bool(value) => if value.value { "true" } else { "false" },
+        Expr.Name(value) => value.name,
+        Expr.Borrow(value) => {
             var prefix = if value.mutable { "&mut " } else { "&" }
             prefix + dump_expr(value.target.value)
         }
-        Expr::Binary(value) => "(" + dump_expr(value.left.value) + " " + value.op + " " + dump_expr(value.right.value) + ")",
-        Expr::Member(value) => dump_expr(value.target.value) + "." + value.member,
-        Expr::Index(value) => dump_expr(value.target.value) + "[" + dump_expr(value.index.value) + "]",
-        Expr::Call(value) => "call " + dump_expr(value.callee.value) + "(" + join_exprs(value.args) + ")",
-        Expr::Match(value) => "match " + dump_expr(value.subject.value) + " { " + join_match_arms(value.arms) + " }",
-        Expr::If(value) => dump_if_expr(value),
-        Expr::While(value) => "while " + dump_expr(value.condition.value) + " {...}",
-        Expr::For(value) => {
+        Expr.Binary(value) => "(" + dump_expr(value.left.value) + " " + value.op + " " + dump_expr(value.right.value) + ")",
+        Expr.Member(value) => dump_expr(value.target.value) + "." + value.member,
+        Expr.Index(value) => dump_expr(value.target.value) + "[" + dump_expr(value.index.value) + "]",
+        Expr.Call(value) => "call " + dump_expr(value.callee.value) + "(" + join_exprs(value.args) + ")",
+        Expr.Match(value) => "match " + dump_expr(value.subject.value) + " { " + join_match_arms(value.arms) + " }",
+        Expr.If(value) => dump_if_expr(value),
+        Expr.While(value) => "while " + dump_expr(value.condition.value) + " {...}",
+        Expr.For(value) => {
             var names = ""
             var i = 0
             while i < value.names.len() {
@@ -496,33 +522,35 @@ func dump_expr(Expr expr) String {
             var decl = if value.declare { " := " } else { " in " }
             "for " + names + decl + dump_expr(value.iterable.value) + " {...}"
         }
-        Expr::Block(_) => "{...}",
-        Expr::Array(value) => {
+        Expr.Block(_) => "{...}",
+        Expr.Array(value) => {
             var elems = Vec[String]()
-            for e in value.items { elems.push(dump_expr(e)) }
+            var _ei = 0
+            while _ei < value.items.len() { elems.push(dump_expr(value.items[_ei])); _ei = _ei + 1 }
             "[" + join_with(elems, ", ") + "]"
         }
-        Expr::Map(value) => {
+        Expr.Map(value) => {
             var parts = Vec[String]()
-            for entry in value.entries { parts.push(dump_expr(entry.key) + ": " + dump_expr(entry.value)) }
+            var _en = 0
+            while _en < value.entries.len() { var entry = value.entries[_en]; parts.push(dump_expr(entry.key) + ": " + dump_expr(entry.value)); _en = _en + 1 }
             "{" + join_with(parts, ", ") + "}"
         }
     }
 }
 
-func dump_if_expr(IfExpr value) String {
+func dump_if_expr(IfExpr value) -> String {
     var text = "if " + dump_expr(value.condition.value) + " {...}"
     match value.else_branch {
-        Option::Some(expr) => text + " else " + dump_expr(expr.value),
-        Option::None => text,
+        Option.Some(expr) => text + " else " + dump_expr(expr.value),
+        Option.None => text,
     }
 }
 
-func dump_pattern(Pattern pattern) String {
+func dump_pattern(Pattern pattern) -> String {
     match pattern {
-        Pattern::Name(value) => value.name,
-        Pattern::Wildcard(_) => "_",
-        Pattern::Variant(value) => {
+        Pattern.Name(value) => value.name,
+        Pattern.Wildcard(_) => "_",
+        Pattern.Variant(value) => {
             if len(value.args) == 0 {
                 return value.path
             }
@@ -531,56 +559,68 @@ func dump_pattern(Pattern pattern) String {
     }
 }
 
-func join_exprs(Vec[Expr] values) String {
+func join_exprs(Vec[Expr] values) -> String {
     var parts = Vec[String]()
-    for value in values {
+    var _iv = 0
+    while _iv < values.len() {
+        var value = values[_iv]
         parts.push(dump_expr(value))
+        _iv = _iv + 1
     }
     join_with(parts, ", ")
 }
 
-func join_patterns(Vec[Pattern] values) String {
+func join_patterns(Vec[Pattern] values) -> String {
     var parts = Vec[String]()
-    for value in values { parts.push(dump_pattern(value)) }
+    var _pv = 0
+    while _pv < values.len() { parts.push(dump_pattern(values[_pv])); _pv = _pv + 1 }
     join_with(parts, ", ")
 }
 
-func join_match_arms(Vec[MatchArm] values) String {
+func join_match_arms(Vec[MatchArm] values) -> String {
     var parts = Vec[String]()
-    for value in values {
+    var _mv = 0
+    while _mv < values.len() {
+        var value = values[_mv]
         parts.push(dump_pattern(value.pattern) + " => " + dump_expr(value.expr))
+        _mv = _mv + 1
     }
     join_with(parts, "; ")
 }
 
-func append_lines(Vec[String] dest, Vec[String] source) () {
-    for line in source {
-        dest.push(line)
+func append_lines(Vec[String] dest, Vec[String] source) -> () {
+    var _li = 0
+    while _li < source.len() {
+        dest.push(source[_li])
+        _li = _li + 1
     }
 }
 
-func single_line(String text) Vec[String] {
+func single_line(String text) -> Vec[String] {
     var lines = Vec[String]()
     lines.push(text)
     lines
 }
 
-func join_lines(Vec[String] lines) String {
+func join_lines(Vec[String] lines) -> String {
     join_with(lines, "\n")
 }
-func join_with(Vec[String] values, String sep) String {
+func join_with(Vec[String] values, String sep) -> String {
     var out = ""
     var first = true
-    for value in values {
+    var _j = 0
+    while _j < values.len() {
+        var value = values[_j]
         if !first {
             out = out + sep
         }
         out = out + value
         first = false
+        _j = _j + 1
     }
     out
 }
 
-func replace_once(String text, String from, String to) String {
+func replace_once(String text, String from, String to) -> String {
     text
 }
