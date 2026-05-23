bea5b61ec271b4cc842e8994d9942b7fef5c3965
shuwen <1201220707@pku.edu.cn>
2026-05-24 00:35:22 +0800

    Prefer native Windows s.exe launcher

--- bin/s.cmd
+++ bin/s.cmd
@@ -1,4 +1,9 @@
 @echo off
 setlocal
+if exist "%~dp0s.exe" (
+    set "PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%PATH%"
+    "%~dp0s.exe" %*
+    exit /b %ERRORLEVEL%
+)
 powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%~dp0s.ps1' --% %*"
 exit /b %ERRORLEVEL%
