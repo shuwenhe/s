79dd117c63d198ffb3aada5a3f8b5b6e84ca5ffe
shuwenhe <1201220707@pku.edu.cn>
2026-08-18 14:37:26 +0800

    refactor: align http type names

--- src/net/http/server.s
+++ src/net/http/server.s
@@ -2,11 +2,11 @@ package src.net.http
 
 struct HTTPServer {
     string addr
-    map[string, func(Request, Response)] routes
+    map[string, func(http_request, http_response)] routes
 }
-func (s *HTTPServer) HandleFunc(string path, func(Request, Response) handler) {
+func (s *HTTPServer) HandleFunc(string path, func(http_request, http_response) handler) {
     if s.routes == nil {
-        s.routes = map[string, func(Request, Response)]{}
+        s.routes = map[string, func(http_request, http_response)]{}
     }
     s.routes[path] = handler
 }
