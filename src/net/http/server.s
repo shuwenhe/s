d0a5172cc2d047998e9ca80b9a577616c7971988
shuwenhe <1201220707@pku.edu.cn>
2026-08-21 17:38:56 +0800

    Format HTTP server

--- src/net/http/server.s
+++ src/net/http/server.s
@@ -4,12 +4,14 @@ struct http_server {
     string addr
     map[string, func(http_request, http_response)] routes
 }
+
 func (http_server* s) handle_func(string path, func(http_request, http_response) handler) {
     if s.routes == nil {
         s.routes = map[string, func(http_request, http_response)]{}
     }
     s.routes[path] = handler
 }
+
 func (http_server* s) listen_and_serve() {
     print("[http] listen on " + s.addr)
 }
